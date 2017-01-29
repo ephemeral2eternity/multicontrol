1aa9c87e8cbf616eb4c98e9715bcf4049eb8b600
Ubuntu <18757@manPlanetlab.jvyjkrkm0i3ezjy3zv3rtqokxd.gx.internal.cloudapp.net>
2017-01-29 22:50:36 +0000

    Add revisions for superbowl 2017

--- multicopy.sh
+++ multicopy.sh
@@ -3,18 +3,19 @@ f=$1
 echo "VM List file: $f"
 srcFile=$2
 dstFile=$3
-keyFile="/home/chenw/.ssh/chenw-theone"
+# keyFile="/home/chenw/.ssh/chenw-theone"
+keyFile="/home/18757/.ssh/18757-s17"
 echo "Copy from location $srcFile to location $dstFile !"
 cat $f | while IFS=, read -r a b; do
 	srvName=$a
 	srvIP=$b
 	if echo "$srcFile" |grep -q "@"; then
-		full_srcFile=$(echo $srcFile| sed s/@/chenw@$srvIP/g)
+		full_srcFile=$(echo $srcFile| sed s/@/18757@$srvIP/g)
 	else
 		full_srcFile=$srcFile
 	fi
 	if echo "$dstFile" |grep "@"; then
-		full_dstFile=$(echo $dstFile| sed s/@/chenw@$srvIP/g)
+		full_dstFile=$(echo $dstFile| sed s/@/18757@$srvIP/g)
 	else
 		full_dstFile=$dstFile
 	fi
--- multiquery.sh
+++ multiquery.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 f=$1
 cmd=$2
-keyFile="/home/chenw/.ssh/chenw-theone"
+# keyFile="/home/chenw/.ssh/chenw-theone"
+keyFile="/home/18757/.ssh/18757-s17"
 cat $f | while IFS=, read -r a b; do
 	srvName=$a
 	srvIP=$b
 	echo "ssh into remote server $srvName with ip $srvIP and execute command $cmd"
-	ssh_cmd="ssh -i $keyFile -n chenw@$srvIP $cmd"
+	ssh_cmd="ssh -i $keyFile -n 18757@$srvIP $cmd"
 	# echo $ssh_cmd
 	$ssh_cmd
 done
