1b95f38e9ac18bb11f0c31c0e906b8ee9cb8602f
Ubuntu <chenw@cmu-agens.lsvyam1dehye3dreswjoz1on4g.gx.internal.cloudapp.net>
2017-04-25 16:50:30 +0000

    add backupdatabase.sh for django agents

---
+++ backupdatabase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+f=$1
+keyFile="/home/chenw/.ssh/chenw-theone"
+suffix=`date +"%m%d" -d now`
+localFolder="/home/chenw/databaseBackup/qrank$suffix/"
+mkdir -p $localFolder
+cat $f | while IFS=, read -r a b c; do
+	srvName=$a
+	srvIP=$b
+	srvFolder=$c
+	cmd="cd ~/$srvFolder;python3 manage.py dumpdata --indent=4 -e sessions -e admin -e contenttypes -e auth --natural-foreign > ~/databaseBackup/$srvName-$suffix.json"
+	echo "ssh into remote server $srvName with ip $srvIP and execute command $cmd"
+	ssh_cmd="ssh -i $keyFile -n chenw@$srvIP $cmd"
+	# echo $ssh_cmd
+	$ssh_cmd
+	scp_cmd="scp -i $keyFile -r chenw@$srvIP:~/databaseBackup/*-$suffix.json $localFolder"
+	echo $scp_cmd
+	$scp_cmd
+done
